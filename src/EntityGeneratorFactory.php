09134daf1dc5758d993dfa314c4ffac86abec6f7
Ambroise Maupate <ambroise@rezo-zero.com>
2020-12-24 00:27:25 +0100

    First commit with moved classes

---
+++ src/EntityGeneratorFactory.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace RZ\Roadiz\EntityGenerator;
+
+use RZ\Roadiz\Contracts\NodeType\NodeTypeInterface;
+use RZ\Roadiz\Contracts\NodeType\NodeTypeResolverInterface;
+
+final class EntityGeneratorFactory
+{
+    /**
+     * @var NodeTypeResolverInterface
+     */
+    private $nodeTypeResolverBag;
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @param NodeTypeResolverInterface $nodeTypeResolverBag
+     * @param array $options
+     */
+    public function __construct(NodeTypeResolverInterface $nodeTypeResolverBag, array $options)
+    {
+        $this->nodeTypeResolverBag = $nodeTypeResolverBag;
+        $this->options = $options;
+    }
+
+    public function create(NodeTypeInterface $nodeType): EntityGenerator
+    {
+        return new EntityGenerator($nodeType, $this->nodeTypeResolverBag, $this->options);
+    }
+}
