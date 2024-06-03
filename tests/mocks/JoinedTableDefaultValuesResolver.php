2a157e2174850ce95e64234a1f7cf2c83a6f74a5
roadiz-ci <ambroise+ci@roadiz.io>
2024-06-03 21:28:34 +0000

    chore: Bumped

--- tests/mocks/JoinedTableDefaultValuesResolver.php
+++ tests/mocks/JoinedTableDefaultValuesResolver.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace RZ\Roadiz\EntityGenerator\Test;
+namespace tests\mocks;
 
 use RZ\Roadiz\Contracts\NodeType\NodeTypeFieldInterface;
 use RZ\Roadiz\EntityGenerator\Field\DefaultValuesResolverInterface;
