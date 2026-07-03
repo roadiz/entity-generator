c8158ea93463cdb8657ab2dd2c8de7f8e853a930
roadiz-ci <ambroise+ci@roadiz.io>
2026-07-03 13:10:48 +0000

    chore: bumped

--- src/Field/ManyToOneFieldGenerator.php
+++ src/Field/ManyToOneFieldGenerator.php
@@ -4,42 +4,40 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\EntityGenerator\Field;
 
-use Nette\PhpGenerator\ClassType;
-use Nette\PhpGenerator\Literal;
-use Nette\PhpGenerator\PhpNamespace;
-use Nette\PhpGenerator\Property;
+use RZ\Roadiz\EntityGenerator\Attribute\AttributeGenerator;
 
-final class ManyToOneFieldGenerator extends AbstractConfigurableFieldGenerator
+class ManyToOneFieldGenerator extends AbstractConfigurableFieldGenerator
 {
-    protected function addFieldAttributes(Property $property, PhpNamespace $namespace, bool $exclude = false): self
+    protected function getFieldAttributes(bool $exclude = false): array
     {
-        parent::addFieldAttributes($property, $namespace, $exclude);
+        $attributes = parent::getFieldAttributes($exclude);
 
         /*
          * Many Users have One Address.
-         * @\Doctrine\ORM\Mapping\ManyToOne(targetEntity="Address")
-         * @\Doctrine\ORM\Mapping\JoinColumn(name="address_id", referencedColumnName="id", onDelete="SET NULL")
+         * @ORM\ManyToOne(targetEntity="Address")
+         * @ORM\JoinColumn(name="address_id", referencedColumnName="id", onDelete="SET NULL")
          */
         $ormParams = [
-            'name' => $this->field->getName().'_id',
-            'referencedColumnName' => 'id',
-            'onDelete' => 'SET NULL',
+            'name' => AttributeGenerator::wrapString($this->field->getName() . '_id'),
+            'referencedColumnName' => AttributeGenerator::wrapString('id'),
+            'onDelete' => AttributeGenerator::wrapString('SET NULL'),
         ];
-        $property->addAttribute('Doctrine\ORM\Mapping\ManyToOne', [
-            'targetEntity' => new Literal($this->getFullyQualifiedClassName().'::class'),
+        $attributes[] = new AttributeGenerator('ORM\ManyToOne', [
+            'targetEntity' => $this->getFullyQualifiedClassName() . '::class'
         ]);
-        $property->addAttribute('Doctrine\ORM\Mapping\JoinColumn', $ormParams);
+        $attributes[] = new AttributeGenerator('ORM\JoinColumn', $ormParams);
 
-        if (true === $this->options['use_api_platform_filters']) {
-            $property->addAttribute('ApiPlatform\Metadata\ApiFilter', [
-                0 => new Literal($namespace->simplifyName('\ApiPlatform\Doctrine\Orm\Filter\SearchFilter').'::class'),
-                'strategy' => 'exact',
+        if ($this->options['use_api_platform_filters'] === true) {
+            $attributes[] = new AttributeGenerator('ApiFilter', [
+                0 => 'OrmFilter\SearchFilter::class',
+                'strategy' => AttributeGenerator::wrapString('exact')
             ]);
         }
 
-        $this->addSerializationAttributes($property);
-
-        return $this;
+        return [
+            ...$attributes,
+            ...$this->getSerializationAttributes()
+        ];
     }
 
     protected function isExcludingFieldFromJmsSerialization(): bool
@@ -47,52 +45,58 @@ final class ManyToOneFieldGenerator extends AbstractConfigurableFieldGenerator
         return false;
     }
 
-    public function addFieldAnnotation(Property $property): self
+    /**
+     * @inheritDoc
+     */
+    public function getFieldAnnotation(): string
     {
-        $this->addFieldAutodoc($property);
-
-        return $this;
+        return '
+    /**
+     *' . implode(PHP_EOL . static::ANNOTATION_PREFIX, $this->getFieldAutodoc()) . '
+     * @var ' . $this->getFullyQualifiedClassName() . '|null
+     */' . PHP_EOL;
     }
 
     protected function getFieldTypeDeclaration(): string
     {
-        return '?'.$this->getFullyQualifiedClassName();
+        return '?' . $this->getFullyQualifiedClassName();
     }
 
-    protected function getFieldDefaultValueDeclaration(): Literal|string|null
+    protected function getFieldDefaultValueDeclaration(): string
     {
-        return new Literal('null');
+        return 'null';
     }
 
-    public function addFieldGetter(ClassType $classType, PhpNamespace $namespace): self
+    /**
+     * @inheritDoc
+     */
+    public function getFieldGetter(): string
     {
-        $classType->addMethod($this->field->getGetterName())
-            ->setReturnType($this->getFieldTypeDeclaration())
-            ->setPublic()
-            ->setBody(<<<PHP
-return \$this->{$this->field->getVarName()};
-PHP
-            );
+        return '
+    /**
+     * @return ' . $this->getFullyQualifiedClassName() . '|null
+     */
+    public function ' . $this->field->getGetterName() . '(): ?' . $this->getFullyQualifiedClassName() . '
+    {
+        return $this->' . $this->field->getVarName() . ';
+    }' . PHP_EOL;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFieldSetter(): string
+    {
+        return '
+    /**
+     * @param ' . $this->getFullyQualifiedClassName() . '|null $' . $this->field->getVarName() . '
+     * @return $this
+     */
+    public function ' . $this->field->getSetterName() . '(?' . $this->getFullyQualifiedClassName() . ' $' . $this->field->getVarName() . ' = null): static
+    {
+        $this->' . $this->field->getVarName() . ' = $' . $this->field->getVarName() . ';
 
         return $this;
-    }
-
-    public function addFieldSetter(ClassType $classType): self
-    {
-        $setter = $classType->addMethod($this->field->getSetterName())
-            ->setReturnType('static')
-            ->addComment('@return $this')
-            ->setPublic();
-        $setter->addParameter($this->field->getVarName())
-            ->setType($this->getFieldTypeDeclaration())
-            ->setNullable();
-
-        $setter->setBody(<<<PHP
-\$this->{$this->field->getVarName()} = \${$this->field->getVarName()};
-return \$this;
-PHP
-        );
-
-        return $this;
+    }' . PHP_EOL;
     }
 }
--- src/Field/ProxiedManyToManyFieldGenerator.php
+++ src/Field/ProxiedManyToManyFieldGenerator.php
@@ -4,57 +4,52 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\EntityGenerator\Field;
 
-use Nette\PhpGenerator\ClassType;
-use Nette\PhpGenerator\Literal;
-use Nette\PhpGenerator\Method;
-use Nette\PhpGenerator\PhpNamespace;
-use Nette\PhpGenerator\Property;
+use RZ\Roadiz\EntityGenerator\Attribute\AttributeGenerator;
+use RZ\Roadiz\EntityGenerator\Attribute\AttributeListGenerator;
 use Symfony\Component\String\UnicodeString;
 
-final class ProxiedManyToManyFieldGenerator extends AbstractConfigurableFieldGenerator
+class ProxiedManyToManyFieldGenerator extends AbstractConfigurableFieldGenerator
 {
-    protected function addSerializationAttributes(Property|Method $property): self
+    protected function getSerializationAttributes(): array
     {
-        parent::addSerializationAttributes($property);
-        if ($this->excludeFromSerialization()) {
-            return $this;
-        }
-
-        $property->addAttribute('JMS\Serializer\Annotation\VirtualProperty');
-        $property->addAttribute('JMS\Serializer\Annotation\SerializedName', [
-            $this->field->getVarName(),
-        ]);
-        $property->addAttribute('Symfony\Component\Serializer\Attribute\SerializedName', [
-            'serializedName' => $this->field->getVarName(),
-        ]);
-        $property->addAttribute('Symfony\Component\Serializer\Attribute\Groups', [
-            $this->getSerializationGroups(),
-        ]);
-        if ($this->getSerializationMaxDepth() > 0) {
-            $property->addAttribute('Symfony\Component\Serializer\Attribute\MaxDepth', [
-                $this->getSerializationMaxDepth(),
+        $annotations = parent::getSerializationAttributes();
+        if (!$this->excludeFromSerialization()) {
+            $annotations[] = new AttributeGenerator('Serializer\VirtualProperty');
+            $annotations[] = new AttributeGenerator('Serializer\SerializedName', [
+                AttributeGenerator::wrapString($this->field->getVarName())
             ]);
+            $annotations[] = new AttributeGenerator('SymfonySerializer\SerializedName', [
+                'serializedName' => AttributeGenerator::wrapString($this->field->getVarName())
+            ]);
+            $annotations[] = new AttributeGenerator('SymfonySerializer\Groups', [
+                $this->getSerializationGroups()
+            ]);
+            if ($this->getSerializationMaxDepth() > 0) {
+                $annotations[] = new AttributeGenerator('SymfonySerializer\MaxDepth', [
+                    $this->getSerializationMaxDepth()
+                ]);
+            }
         }
-
-        return $this;
+        return $annotations;
     }
 
     /**
      * Generate PHP property declaration block.
      */
-    protected function getFieldProperty(ClassType $classType): Property
+    protected function getFieldDeclaration(): string
     {
-        return $classType
-            ->addProperty($this->getProxiedVarName())
-            ->setPrivate()
-            ->addComment('Buffer var to get referenced entities (documents, nodes, cforms, doctrine entities)')
-            ->setType('\Doctrine\Common\Collections\Collection');
+        /*
+         * Buffer var to get referenced entities (documents, nodes, cforms, doctrine entities)
+         */
+        return '    private Collection $' . $this->getProxiedVarName() . ';' . PHP_EOL;
     }
 
-    protected function addFieldAttributes(Property $property, PhpNamespace $namespace, bool $exclude = false): self
+    protected function getFieldAttributes(bool $exclude = false): array
     {
-        $property->addAttribute('JMS\Serializer\Annotation\Exclude');
-        $property->addAttribute('Symfony\Component\Serializer\Attribute\Ignore');
+        $attributes = [];
+
+        $attributes[] = new AttributeGenerator('Serializer\Exclude');
+        $attributes[] = new AttributeGenerator('SymfonySerializer\Ignore');
 
         /*
          * Many Users have Many Groups.
@@ -64,169 +59,191 @@ final class ProxiedManyToManyFieldGenerator extends AbstractConfigurableFieldGen
          *      inverseJoinColumns={@JoinColumn(name="group_id", referencedColumnName="id")}
          */
         $ormParams = [
-            'targetEntity' => new Literal('\\'.trim($this->getProxyClassname(), '\\').'::class'),
-            'mappedBy' => $this->configuration['proxy']['self'],
-            'orphanRemoval' => true,
-            'cascade' => ['persist', 'remove'],
+            'targetEntity' => '\\' . trim($this->getProxyClassname(), '\\') . '::class',
+            'mappedBy' => AttributeGenerator::wrapString($this->configuration['proxy']['self']),
+            'orphanRemoval' => 'true',
+            'cascade' => '["persist", "remove"]'
         ];
 
-        $property->addAttribute('Doctrine\ORM\Mapping\OneToMany', $ormParams);
+        $attributes[] = new AttributeGenerator('ORM\OneToMany', $ormParams);
 
         if (isset($this->configuration['proxy']['orderBy']) && count($this->configuration['proxy']['orderBy']) > 0) {
             // use default order for Collections
             $orderBy = [];
             foreach ($this->configuration['proxy']['orderBy'] as $order) {
-                $orderBy[$order['field']] = $order['direction'];
+                $orderBy[] = AttributeGenerator::wrapString($order['field']) .
+                    ' => ' .
+                    AttributeGenerator::wrapString($order['direction']);
             }
-            $property->addAttribute('Doctrine\ORM\Mapping\OrderBy', [
-                $orderBy,
+            $attributes[] = new AttributeGenerator('ORM\OrderBy', [
+                0 => '[' . implode(', ', $orderBy) . ']'
             ]);
         }
 
-        return $this;
+        return $attributes;
     }
 
-    public function addFieldAnnotation(Property $property): self
+
+    /**
+     * @inheritDoc
+     */
+    public function getFieldAnnotation(): string
     {
-        $property->addComment($this->field->getLabel().'.');
-        $property->addComment('@var \Doctrine\Common\Collections\Collection<int, '.$this->getProxyClassname().'>');
+        return '
+    /**
+     * ' . $this->field->getLabel() . '
+     *
+     * @var Collection<int, ' . $this->getProxyClassname() . '>
+     */' . PHP_EOL;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFieldGetter(): string
+    {
+        return '
+    /**
+     * @return Collection<int, ' . $this->getProxyClassname() . '>
+     */
+    public function ' . $this->getProxiedGetterName() . '(): Collection
+    {
+        return $this->' . $this->getProxiedVarName() . ';
+    }
+
+    /**
+     * @return Collection
+     */
+' . (new AttributeListGenerator($this->getSerializationAttributes()))->generate(4) . '
+    public function ' . $this->field->getGetterName() . '(): Collection
+    {
+        return $this->' . $this->getProxiedVarName() . '->map(function (' . $this->getProxyClassname() . ' $proxyEntity) {
+            return $proxyEntity->' . $this->getProxyRelationGetterName() . '();
+        });
+    }' . PHP_EOL;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFieldSetter(): string
+    {
+        return '
+    /**
+     * @param Collection $' . $this->getProxiedVarName() . '
+     * @Serializer\VirtualProperty()
+     * @return $this
+     */
+    public function ' . $this->getProxiedSetterName() . '(Collection $' . $this->getProxiedVarName() . '): static
+    {
+        $this->' . $this->getProxiedVarName() . ' = $' . $this->getProxiedVarName() . ';
 
         return $this;
     }
-
-    public function addFieldGetter(ClassType $classType, PhpNamespace $namespace): self
+    /**
+     * @param Collection|array|null $' . $this->field->getVarName() . '
+     * @return $this
+     */
+    public function ' . $this->field->getSetterName() . '(Collection|array|null $' . $this->field->getVarName() . ' = null): static
     {
-        $classType->addMethod($this->getProxiedGetterName())
-            ->setReturnType('\Doctrine\Common\Collections\Collection')
-            ->setPublic()
-            ->setBody('return $this->'.$this->getProxiedVarName().';')
-            ->addComment(
-                '@return '.
-                $namespace->simplifyName('\Doctrine\Common\Collections\Collection').
-                '<int, '.
-                $this->getProxyClassname().
-                '>'
-            );
-
-        // Real getter
-        $getter = $classType->addMethod($this->field->getGetterName())
-            ->setPublic()
-            ->setReturnType('array');
-        $this->addSerializationAttributes($getter);
-        $getter->setBody(<<<EOF
-return \$this->{$this->getProxiedVarName()}->map(function ({$this->getProxyClassname()} \$proxyEntity) {
-    return \$proxyEntity->{$this->getProxyRelationGetterName()}();
-})->getValues();
-EOF
-        );
-
-        return $this;
-    }
-
-    public function addFieldSetter(ClassType $classType): self
-    {
-        $proxySetter = $classType->addMethod($this->getProxiedSetterName())
-            ->setReturnType('static')
-            ->setPublic()
-            ->addComment('@param \Doctrine\Common\Collections\Collection<int, '.$this->getProxyClassname().'> $'.$this->getProxiedVarName())
-            ->addComment('@return $this')
-        ;
-        $proxySetter->addParameter($this->getProxiedVarName())
-            ->setType('\Doctrine\Common\Collections\Collection');
-
-        $proxySetter->setBody(<<<EOF
-\$this->{$this->getProxiedVarName()} = \${$this->getProxiedVarName()};
-return \$this;
-EOF
-        );
-
-        $setter = $classType->addMethod($this->field->getSetterName())
-            ->setReturnType('static')
-            ->setPublic()
-            ->addComment('@return $this')
-        ;
-        $setter->addParameter($this->field->getVarName())
-            ->setType('\Doctrine\Common\Collections\Collection|array|null');
-
-        $ucFieldVarName = ucwords($this->field->getVarName());
-
-        $setter->setBody(<<<EOF
-foreach (\$this->{$this->getProxiedGetterName()}() as \$item) {
-    \$item->{$this->getProxySelfSetterName()}(null);
-}
-\$this->{$this->getProxiedVarName()}->clear();
-if (null !== \${$this->field->getVarName()}) {
-    \$position = 0;
-    foreach (\${$this->field->getVarName()} as \$single{$ucFieldVarName}) {
-        \$proxyEntity = new {$this->getProxyClassname()}();
-        \$proxyEntity->{$this->getProxySelfSetterName()}(\$this);
-        if (\$proxyEntity instanceof \RZ\Roadiz\Core\AbstractEntities\PositionedInterface) {
-            \$proxyEntity->setPosition(++\$position);
+        foreach ($this->' . $this->getProxiedGetterName() . '() as $item) {
+            $item->' . $this->getProxySelfSetterName() . '(null);
         }
-        \$proxyEntity->{$this->getProxyRelationSetterName()}(\$single{$ucFieldVarName});
-        \$this->{$this->getProxiedVarName()}->add(\$proxyEntity);
-    }
-}
-
-return \$this;
-EOF
-        );
+        $this->' . $this->getProxiedVarName() . '->clear();
+        if (null !== $' . $this->field->getVarName() . ') {
+            $position = 0;
+            foreach ($' . $this->field->getVarName() . ' as $single' . ucwords($this->field->getVarName()) . ') {
+                $proxyEntity = new ' . $this->getProxyClassname() . '();
+                $proxyEntity->' . $this->getProxySelfSetterName() . '($this);
+                if ($proxyEntity instanceof \RZ\Roadiz\Core\AbstractEntities\PositionedInterface) {
+                    $proxyEntity->setPosition(++$position);
+                }
+                $proxyEntity->' . $this->getProxyRelationSetterName() . '($single' . ucwords($this->field->getVarName()) . ');
+                $this->' . $this->getProxiedVarName() . '->add($proxyEntity);
+                $this->objectManager->persist($proxyEntity);
+            }
+        }
 
         return $this;
+    }' . PHP_EOL;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldConstructorInitialization(): string
     {
-        return '$this->'.$this->getProxiedVarName().' = new \Doctrine\Common\Collections\ArrayCollection();';
+        return '$this->' . $this->getProxiedVarName() . ' = new \Doctrine\Common\Collections\ArrayCollection();';
     }
 
+    /**
+     * @return string
+     */
     protected function getProxiedVarName(): string
     {
-        return $this->field->getVarName().'Proxy';
+        return $this->field->getVarName() . 'Proxy';
     }
-
+    /**
+     * @return string
+     */
     protected function getProxiedSetterName(): string
     {
-        return $this->field->getSetterName().'Proxy';
+        return $this->field->getSetterName() . 'Proxy';
     }
-
+    /**
+     * @return string
+     */
     protected function getProxiedGetterName(): string
     {
-        return $this->field->getGetterName().'Proxy';
+        return $this->field->getGetterName() . 'Proxy';
     }
-
+    /**
+     * @return string
+     */
     protected function getProxySelfSetterName(): string
     {
-        return 'set'.ucwords($this->configuration['proxy']['self']);
+        return 'set' . ucwords($this->configuration['proxy']['self']);
     }
-
+    /**
+     * @return string
+     */
     protected function getProxyRelationSetterName(): string
     {
-        return 'set'.ucwords($this->configuration['proxy']['relation']);
+        return 'set' . ucwords($this->configuration['proxy']['relation']);
     }
-
+    /**
+     * @return string
+     */
     protected function getProxyRelationGetterName(): string
     {
-        return 'get'.ucwords($this->configuration['proxy']['relation']);
+        return 'get' . ucwords($this->configuration['proxy']['relation']);
     }
 
+    /**
+     * @return string
+     */
     protected function getProxyClassname(): string
     {
         return (new UnicodeString($this->configuration['proxy']['classname']))->startsWith('\\') ?
             $this->configuration['proxy']['classname'] :
-            '\\'.$this->configuration['proxy']['classname'];
+            '\\' . $this->configuration['proxy']['classname'];
     }
 
+    /**
+     * @return string
+     */
     public function getCloneStatements(): string
     {
-        return <<<PHP
-\${$this->getProxiedVarName()}Clone = new \Doctrine\Common\Collections\ArrayCollection();
-foreach (\$this->{$this->getProxiedVarName()} as \$item) {
-    \$itemClone = clone \$item;
-    \$itemClone->{$this->getProxySelfSetterName()}(\$this);
-    \${$this->getProxiedVarName()}Clone->add(\$itemClone);
-}
-\$this->{$this->getProxiedVarName()} = \${$this->getProxiedVarName()}Clone;
-PHP;
+        return '
+
+        $' . $this->getProxiedVarName() . 'Clone = new \Doctrine\Common\Collections\ArrayCollection();
+        foreach ($this->' . $this->getProxiedVarName() . ' as $item) {
+            $itemClone = clone $item;
+            $itemClone->setNodeSource($this);
+            $' . $this->getProxiedVarName() . 'Clone->add($itemClone);
+            $this->objectManager->persist($itemClone);
+        }
+        $this->' . $this->getProxiedVarName() . ' = $' . $this->getProxiedVarName() . 'Clone;
+        ';
     }
 }
